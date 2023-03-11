0524ec9fa81abafa3cca91341080fcbf78a3e9e7
Evgeny Filatov <evgenybf@gmail.com>
2023-03-11 18:55:57 +0300

    Initial fullscreen mode support

--- desktop/src/org/stransball/desktop/DesktopLauncher.java
+++ desktop/src/org/stransball/desktop/DesktopLauncher.java
@@ -3,17 +3,38 @@ package org.stransball.desktop;
 import org.stransball.Constants;
 import org.stransball.GameMain;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3WindowAdapter;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        // System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
-        config.setTitle("Just an experiment");
-        config.setForegroundFPS(60);
-        config.setWindowedMode(Constants.DEFAULT_SCREEN_WIDTH, Constants.DEFAULT_SCREEN_HEIGHT);
+
+        config.setTitle("SuperTransball GDX");
+        config.setForegroundFPS(200);
+
+        config.setWindowedMode(Constants.DEFAULT_SCREEN_WIDTH, Constants.DEFAULT_SCREEN_HEIGHT / 2);
+
+        config.setResizable(true);
         config.useVsync(true);
+        config.setAutoIconify(true);
+
+        config.setWindowListener(new Lwjgl3WindowAdapter() {
+            @Override
+            public void maximized(boolean isMaximized) {
+                if (isMaximized)
+                    Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+                else
+                    Gdx.graphics.setWindowedMode(Constants.DEFAULT_SCREEN_WIDTH, Constants.DEFAULT_SCREEN_HEIGHT);
+            }
+
+            @Override
+            public void focusLost() {
+                Gdx.app.postRunnable(() -> maximized(false));
+            }
+        });
 
         new Lwjgl3Application(new GameMain(), config);
     }
